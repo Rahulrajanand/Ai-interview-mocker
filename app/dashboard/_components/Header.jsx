6edd1a1d936fc5c5de258b7e22ae5d380b10fd2d
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-09-30 23:10:15 +0530

    updated

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -46,3 +46,53 @@ function Header() {
 }
 
 export default Header
+
+
+// function Header() {
+//   const path = usePathname();
+
+//   useEffect(() => {
+//       console.log(path);
+//   }, [path]);
+
+//   return (
+//       <div className='flex p-2 items-center justify-between bg-secondary shadow-sm'>
+//           {/* Spacer to replace the image */}
+//           <div style={{ width: '100px', height: '100px' }}></div>
+          
+//           <ul className='hidden md:flex gap-6'>
+//               <Link href={"/dashboard"}>
+//                   <li className={`hover:text-primary hover:font-bold transition-all
+//                   cursor-pointer
+//                   ${path == '/dashboard' && 'text-primary font-bold'}
+//                   `}>
+//                       Dashboard
+//                   </li>
+//               </Link>
+//               <li className={`hover:text-primary hover:font-bold transition-all
+//               cursor-pointer
+//               ${path == '/dashboard/questions' && 'text-primary font-bold'}
+//               `}>
+//                   Questions
+//               </li>
+//               <Link href={"/dashboard/upgrade"}>
+//                   <li className={`hover:text-primary hover:font-bold transition-all
+//                   cursor-pointer
+//                   ${path == '/dashboard/upgrade' && 'text-primary font-bold'}
+//                   `}>
+//                       Upgrade
+//                   </li>
+//               </Link>
+//               <li className={`hover:text-primary hover:font-bold transition-all
+//               cursor-pointer
+//               ${path == '/dashboard/how' && 'text-primary font-bold'}
+//               `}>
+//                   How it Works?
+//               </li>
+//           </ul>
+//           <UserButton />
+//       </div>
+//   );
+// }
+
+// export default Header;
